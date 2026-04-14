50846829e7430b5d93cc77bcdea5c4de89453d5e
Obenganim <felixanim552@gmail.com>
2026-04-14 14:27:57 +0000

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@ const settings = {
   author: '‎',
   botName: "BLIFORST",
   botOwner: 'Spartan', // Your name
-  ownerNumber: '‬', //Set your number here without + symbol, just add country code & number without any space
+  ownerNumber: '18768590048‬‬', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   maxStoreMessages: 20, 
